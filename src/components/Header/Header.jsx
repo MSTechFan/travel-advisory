bcf83947738a6878f1d677fd9011d40d817b68e8
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-03-08 16:57:17 -0500

    29.36 RapidAPI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { AutoComplete } from '@react-google-maps/api'
+import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
+import searchIcon from '@material-ui/icons/Search'
+
+import useStyles from './styles'
 
 const Header = () => {
+  const classes = useStyles()
   return (
-    <div>Header</div>
+    <AppBar position='static'>
+      <toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explor new places
+          </Typography>
+          {/* <AutoComplete> */}
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <searchIcon/>
+              </div>
+              <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input:classes.inputInput}}/>
+            </div>
+          {/* </AutoComplete> */}
+        </Box>
+      </toolbar>
+    </AppBar>
   )
 }
 

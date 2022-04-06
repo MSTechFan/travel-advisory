c390658b9e98519f88f4e2e1993f45106d72fedc
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-04-06 16:05:22 -0500

    1.36 missing styles

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import {Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@mui/material'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import PhoneIcon from '@mui/icons-material/Phone'
+import Rating from '@mui/lab/Rating'
+
+import useStyles from './styles'
 
 const PlaceDetails = ({ place }) => {
+  const classes = useStyles()
+
   return (
-    <div>
-      <h1>{place.name}</h1>
-    </div>
+      <Card elevation={6}>
+        <CardMedia
+          style={{height: 350}}
+          image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+          title={place.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">{place.name}</Typography>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Price</Typography>
+            <Typography variant="subtitle1">Price</Typography>
+
+          </Box>
+        </CardContent>
+      </Card>
   )
 }
 

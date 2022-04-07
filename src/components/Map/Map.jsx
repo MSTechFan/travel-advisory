437396ceb2ea89df31799007b4c8269c0a21f47c
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-04-07 15:54:57 -0500

    1.27.36 First Application

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ import Rating from '@mui/lab'
 
 import useStyles from './styles'
 
-const Map = ({setCoordinates, setBounds, coordinates}) => {
+const Map = ({setCoordinates, setBounds, coordinates, places}) => {
   const classes = useStyles()
   const isMobile = useMediaQuery('(min-width:600px)')
 
@@ -25,9 +25,33 @@ const Map = ({setCoordinates, setBounds, coordinates}) => {
           setCoordinates({lat: event.center.lat, lng: event.center.lng})
           setBounds({ne: event.marginBounds.ne, sw: event.marginBounds.sw})
         }}
-        onChildClick={''}
-        
+        onChildClick={''}        
+      >
+      {places?.map((place, i) => (
+        <div 
+          className={classes.markerContainer}
+          lat={Number(place.latitude)}
+          lng={Number(place.longitude)}
+          key={i}
         >
+          {
+            isMobile ? (
+              <LocationOnOutlined color="primary" fontSize="large" />
+            ) : (
+              <Paper eleveation={3} className={classes.paper}>
+                <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                  {place.name}
+                </Typography>
+                <img 
+                  className={classes.pointer}
+                  src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  alt={place.name}
+                />
+              </Paper>
+            )
+          }
+        </div>
+      ))}
 
       </GoogleMapReact>
     </div>
